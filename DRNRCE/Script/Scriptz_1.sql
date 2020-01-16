61e3e0fe1cfdda6e2e3d5ebf3f234addf53a9240
cpnvbenoit <47849605+cpnvbenoit@users.noreply.github.com>
2020-01-16 15:57:39 +0100

    ajout script

--- DRNRCE/Script/Scriptz_1.sql
+++ DRNRCE/Script/Scriptz_1.sql
@@ -1,3 +1,5 @@
+USE master
+go
 IF db_id('drone') IS NOT NULL
 BEGIN
     drop database drone
@@ -8,19 +10,25 @@ CREATE DATABASE drone
 	-- ( NAME = 'drone_log', FILENAME = 'C:\Users\benoit.pierrehumbert\Documents\GitHub\MA-08_Drone\DRNRCE\Script\log\drone.mdf' , SIZE = 20480KB , MAXSIZE = 51200KB, FILEGROWTH = 1024KB )
 	-- LOG ON 
 	-- ( NAME = 'drone_log', FILENAME = 'C:\Users\benoit.pierrehumbert\Documents\GitHub\MA-08_Drone\DRNRCE\Script\log\drone_log.ldf' , SIZE = 10240KB , MAXSIZE = 20480KB , FILEGROWTH = 1024KB)
-	
+USE drone 
+go	
 create table customers (
 	id int identity(1,1) PRIMARY KEY,
 	lastname varchar(100) NOT NULL,
 	firstname varchar(60) NOT NULL,
 	register_date DATE NOT NULL,
+	zip INT NOT NULL,
+	address VARCHAR(100) NOT NULL,
+	email_address VARCHAR(100) NOT NULL,
+	country VARCHAR(80) NOT NULL
 )
 
 create table orders (
 	id int identity(1,1) PRIMARY KEY,
 	date DATE NOT NULL,
 	status varchar(45) NOT NULL,
-	customerid int FOREIGN KEY REFERENCES customers(id)
+	customerid int FOREIGN KEY REFERENCES customers(id),
+	serial_number INT NOT NULL
 )
 
 create table products (
@@ -32,12 +40,7 @@ create table products (
 	stock INT NOT NULL
 )
 
-create table categories (
-	id int identity(1,1) PRIMARY KEY,
-	type varchar(45) NOT NULL
-)
-
-create table suppliers (
+create table providers (
 	id int identity(1,1) PRIMARY KEY,
 	name varchar(100) NOT NULL,
 	nationality varchar(80) NOT NULL,
@@ -58,24 +61,22 @@ create table employees (
 	lastname varchar(100) NOT NULL,
 	firstname varchar(60) NOT NULL,
 	address varchar(150) NOT NULL,
-	phonenumber INT
+	phonenumber INT,
+	zip INT NOT NULL,
+	post VARCHAR(80) NOT NULL,
+	email_address VARCHAR(100)
+	
 )
 
-create table contain (
+create table contain(
 	id int identity(1,1) PRIMARY KEY,
 	orders_id int FOREIGN KEY REFERENCES orders(id),
 	products_id int FOREIGN KEY REFERENCES products(id)
 )
 
-create table define(
-	id int identity(1,1) PRIMARY KEY,
-	categories_id int FOREIGN KEY REFERENCES categories(id),
-	products_id int FOREIGN KEY REFERENCES products(id)
-)
-
 create table provide(
 	id int identity(1,1) PRIMARY KEY,
-	suppliers_id int FOREIGN KEY REFERENCES suppliers(id),
+	providers_id int FOREIGN KEY REFERENCES providers(id),
 	products_id int FOREIGN KEY REFERENCES products(id)
 )
 
